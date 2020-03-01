aa382776954f94e54f73a01eb2e70244dc8c96e2
micos <micos7@gmail.com>
2020-03-01 13:18:40 +0200

    login api

--- app/src/main/java/com/example/hackernews/HackerNewsApi.java
+++ app/src/main/java/com/example/hackernews/HackerNewsApi.java
@@ -1,10 +1,15 @@
 package com.example.hackernews;
 
+import org.xml.sax.helpers.DefaultHandler;
+
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface HackerNewsApi {
 
@@ -14,4 +19,8 @@ public interface HackerNewsApi {
 
     @GET("item/{id}.json?print=pretty")
     Call<DataResponse> getStory(@Path("id") Integer id);
+
+    @POST("login?acct={user}&pw={password}")
+    Call<DefaultHandler> login(@Query("user") String user, @Query("password") String password);
+
 }
